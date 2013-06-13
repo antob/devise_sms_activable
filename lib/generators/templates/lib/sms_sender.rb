49af3f059c0861ef1beb0e59c75941234d7e2e06
Ivan Khromov <ivan@khromov.net>
2013-06-13 23:08:17 +0600

    MainSMS API added

--- lib/generators/templates/lib/sms_sender.rb
+++ lib/generators/templates/lib/sms_sender.rb
@@ -2,8 +2,13 @@ module Devise
   class SmsSender
     #Actually sends the sms token. feel free to modify and adapt to your provider and/or gem
     def self.send_sms(phone,message)
-      sms = Moonshado::Sms.new(phone, message)
-      return sms.deliver_sms[:stat]
+      message = MainsmsApi::Message.new(sender: I18n.t('app_from'), message: message, recipients: [phone])
+      response = message.deliver
+      if response.status == 'success'
+        true
+      else
+        false
+      end
     end   
   end
 end
